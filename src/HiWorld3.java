c08cef4d3a41ad552b45c2c90d86d4d3809c4138
jq7888 <2058878376@qq.com>
2020-12-30 22:55:51 +0800

    测试3第一次提交

---
+++ src/HiWorld3.java
@@ -0,0 +1,13 @@
+/**
+ * ClassName:HiWorld3
+ * Package:PACKAGE_NAME
+ * Description:
+ *
+ * @Date:2020/12/30 22:49
+ * @Author:jiaqi@163.com
+ */
+public class HiWorld3 {
+    public static void main(String[] args) {
+        System.out.println("Hi World 3  !");
+    }
+}
